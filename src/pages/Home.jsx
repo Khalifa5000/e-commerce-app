bb10f310ea9ebd6e88a3236877c033a1d03f7541
Mohamed Khalifa <106628515+Khalifa5000@users.noreply.github.com>
2026-06-21 22:03:11 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
+
+import Img from '../assets/e-commerce.jpg';  
+import './Home.css';
 function Home() {
   return (
     <div>
       <h1>أهلاً بك في المتجر</h1>
+      <p>تصفح منتجاتنا واستمتع بتجربة تسوق رائعة!</p>
+
+      <div>
+      <img className="store-image" src={Img} alt="صورة المتجر" />
+      </div>
+      <button>تسوق الآن</button>
+
     </div>
   );
 }
